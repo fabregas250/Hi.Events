444b6e9a758251d754eb0f5ce9146d9a7179eb05
MUNEZERO BARAKA FABREGAS <130171250+fabregas250@users.noreply.github.com>
2025-02-21 12:30:04 +0200

    Update index.tsx

--- frontend/src/components/common/PoweredByFooter/index.tsx
+++ frontend/src/components/common/PoweredByFooter/index.tsx
@@ -27,10 +27,10 @@ export const PoweredByFooter = (props: React.DetailedHTMLProps<React.HTMLAttribu
             <div className={classes.poweredByText}>
                 {t`Powered by`}{'  '}
                 {/* eslint-disable-next-line lingui/no-unlocalized-strings */}
-                <a href="https://hi.events?utm_source=app-powered-by-footer"
+                <a href="https://dtecsoftwaresolutions.com"
                    target="_blank"
-                   title={'Effortlessly manage events and sell tickets online with Hi.Events'}>
-                    Hi.Events
+                   title={'Effortlessly manage events and sell tickets online'}>
+                    DTEC Software Solutions
                 </a> 🚀
             </div>
         </div>
